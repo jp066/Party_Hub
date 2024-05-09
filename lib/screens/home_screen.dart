998b150c815af0ee1da66d0bc1c5e1f61a227361
CamillyFeitosaa <euuucamilly18feitosa@gmail.com>
2024-05-09 12:16:25 -0300

    search page

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,15 +1,19 @@
 import 'package:flutter/material.dart';
-//import 'package:sos_central/widgets/bottom_bar.dart';
+import 'package:sos_central/theme/app_colors.dart';
+import 'package:sos_central/theme/main_page.dart';
 
 class HomeScreen extends StatelessWidget {
-  @override
+  const HomeScreen ({super.key});
+
+  @override 
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-        ),
+    return MaterialApp(
+      title: 'Sos Central',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+        primaryColor: AppColors.orangeSwatch
       ),
+      home: const MainPage()
     );
   }
 }
